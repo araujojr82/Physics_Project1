baabc033ac1776eabc106129baff6aa220710e7c
Euclides Araujo <araujojr@gmail.com>
2017-10-14 02:42:12 -0400

    ADD more realistic collision velocity calculation :)

--- FirstOpenGL/Physics.cpp
+++ FirstOpenGL/Physics.cpp
@@ -26,56 +26,34 @@ void bounceSpheres( cGameObject* pA, cGameObject* pB )
 {
 	glm::vec3 nv1; // new velocity for sphere 1
 	glm::vec3 nv2; // new velocity for sphere 2
-				// this can probably be optimised a bit, but it basically swaps the velocity amounts
-				// that are perpendicular to the surface of the collistion.
-				// If the spheres had different masses, then u would need to scale the amounts of
-				// velocities exchanged inversely proportional to their masses.
 
-	nv1 = pA->vel;
-	nv1 += projectUonV( pB->vel, ( pB->position - pA->position ) );
-	nv1 -= projectUonV( pA->vel, ( pA->position - pB->position ) );
+	float deltaMass, deltaVelX_2, a,
+		deltaPosX, deltaPosZ, 
+		deltaVelX, deltaVelZ, 
+		velX_cm, velZ_cm;
 
-	nv2 = pB->vel;
-	nv2 += projectUonV( pA->vel, ( pB->position - pA->position ) );
-	nv2 -= projectUonV( pB->vel, ( pA->position - pB->position ) );
+	deltaMass = pB->mass / pA->mass;
+	deltaPosX = pB->position.x - pA->position.x;
+	deltaPosZ = pB->position.z - pA->position.z;
+	deltaVelX = pB->vel.x - pA->vel.x;
+	deltaVelZ = pB->vel.z - pA->vel.z;
+
+	velX_cm = ( pA->mass * pA->vel.x + pB->mass * pB->vel.x ) / ( pA->mass + pB->mass );
+	velZ_cm = ( pA->mass * pA->vel.z + pB->mass * pB->vel.z ) / ( pA->mass + pB->mass );
+
+	// Check division by zero
+	if( deltaPosX == 0.0f )     a = deltaPosZ;
+	else						a = deltaPosZ / deltaPosX;
+
+
+	deltaVelX_2 = -2 * ( deltaVelX + a * deltaVelZ ) / ( ( 1 + a * a ) * ( 1 + deltaMass ) );
 	
-	pA->vel = nv1;
-	pB->vel = nv2;
+	// Update the velocities of X and Z for pA and pB
+	pA->vel.x -= deltaMass * deltaVelX_2;
+	pA->vel.z -= a  * deltaMass * deltaVelX_2;
+
+	pB->vel.x += deltaVelX_2;
+	pB->vel.z += a * deltaVelX_2;
 
 	return;
-}
-
-glm::vec3 scale( const glm::vec3& v, glm::vec3 a ) {
-	glm::vec3 r;
-	r.x = v.x * a.x;
-	//r.y = v.y * a.y;
-	r.y = 1.0f;
-	r.z = v.z * a.z;
-	return r;
-}
-
-glm::vec3 projectUonV( const glm::vec3& u, const glm::vec3& v ) {
-	glm::vec3 r;
-
-	//r = scale( v, ( u * v ) / ( v * v ) );
-
-	// Create some temporary vectors to check for zero division	
-	glm::vec3 tempVec3_a;
-	glm::vec3 tempVec3_a1;
-	glm::vec3 tempVec3_a2;
-
-	tempVec3_a1 = ( u * v );
-	tempVec3_a2 = ( v * v );
-
-	if( tempVec3_a2.x != 0.0f )	tempVec3_a.x = tempVec3_a1.x / tempVec3_a2.x;
-	else						tempVec3_a.x = 0.0f; // tempVec3_a1.x;
-	if( tempVec3_a2.y != 0.0f )	tempVec3_a.y = tempVec3_a1.y / tempVec3_a2.y;
-	else						tempVec3_a.y = 0.0f; // tempVec3_a1.y;
-	if( tempVec3_a2.z != 0.0f )	tempVec3_a.z = tempVec3_a1.z / tempVec3_a2.z;
-	else						tempVec3_a.z = 0.0f; // tempVec3_a1.z;
-
-	r = scale( v, tempVec3_a );
-	
-
-	return r;
 }
